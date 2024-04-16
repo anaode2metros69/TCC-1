b4534d1e7dab7cb428dfaf3dfae59b8ec90724a3
anaode2metros69 <163211660+anaode2metros69@users.noreply.github.com>
2024-04-16 20:02:50 -0300

    Add files via upload
    projeto com o blog

---
+++ Mulheres no espectro/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider-principal').slick({
+        dots: true,
+        infinite: false,
+        speed: 3000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: false,
+        autoplaySpeed: 1,
+    });
+});
